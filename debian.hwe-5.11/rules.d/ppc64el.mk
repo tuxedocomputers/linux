c6469e5962e36acaf53ceaccea7afca567b78431
Andrea Righi <andrea.righi@canonical.com>
2021-04-19 17:33:12 +0200

    UBUNTU: [Packaging] Remove unused 'loader' variable from build rules
    The use of the 'loader' variable was removed back in 2017 in Bionic with
    commit fbf2867282ee ("UBUNTU: [Packaging] update to Debian like control
    scripts") so drop it going forward.
    
    Ignore: yes
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian.hwe-5.11/rules.d/ppc64el.mk
+++ debian.hwe-5.11/rules.d/ppc64el.mk
@@ -8,7 +8,6 @@ kernel_file     = arch/powerpc/boot/vmlinux.strip
 install_file	= vmlinux
 no_dumpfile	= true
 vdso		= vdso_install
-loader		= grub
 do_extras_package = true
 opal_signed       = true
 do_tools_usbip    = true
