265798aa7e42758dabb6426e088f92385dc705d8
Xie Yongji <xieyongji@bytedance.com>
2021-07-15 16:00:25 +0800

    vDPA/ifcvf: Fix return value check for vdpa_alloc_device()
    BugLink: https://bugs.launchpad.net/bugs/1943756
    
    [ Upstream commit 1057afa0121db8bd3ca4718c8e0ca12388ab7759 ]
    
    The vdpa_alloc_device() returns an error pointer upon
    failure, not NULL. To handle the failure correctly, this
    replaces NULL check with IS_ERR() check and propagate the
    error upwards.
    
    Fixes: 5a2414bc454e ("virtio: Intel IFC VF driver for VDPA")
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Xie Yongji <xieyongji@bytedance.com>
    Link: https://lore.kernel.org/r/20210715080026.242-3-xieyongji@bytedance.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/vdpa/ifcvf/ifcvf_main.c
+++ drivers/vdpa/ifcvf/ifcvf_main.c
@@ -433,9 +433,9 @@ static int ifcvf_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	adapter = vdpa_alloc_device(struct ifcvf_adapter, vdpa,
 				    dev, &ifc_vdpa_ops,
 				    IFCVF_MAX_QUEUE_PAIRS * 2);
-	if (adapter == NULL) {
+	if (IS_ERR(adapter)) {
 		IFCVF_ERR(pdev, "Failed to allocate vDPA structure");
-		return -ENOMEM;
+		return PTR_ERR(adapter);
 	}
 
 	pci_set_master(pdev);
