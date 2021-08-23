564585aa9df4823fa6aea6168c7e389f11666178
Andrea Righi <andrea.righi@canonical.com>
2021-08-23 17:17:20 +0200

    UBUNTU: [Config] re-enable AUFS
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian.hwe-5.13/rules.d/hooks.mk
+++ debian.hwe-5.13/rules.d/hooks.mk
@@ -19,3 +19,7 @@ do_libc_dev_package	= false
 do_doc_package		= false
 do_tools_common		= false
 do_tools_host		= false
+do_libc_dev_package	= false
+do_doc_package		= false
+do_tools_common		= false
+do_tools_host		= false
