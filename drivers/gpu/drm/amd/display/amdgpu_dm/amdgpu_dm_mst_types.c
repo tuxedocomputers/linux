73ba2ea250654385b47fce7679ddd47e2e1755be
Hersen Wu <hersenwu@amd.com>
2021-08-25 16:27:47 -0400

    drm/amd/display: dsc mst 2 4K displays go dark with 2 lane HBR3
    BugLink: https://bugs.launchpad.net/bugs/1948485
    
    commit 90517c9838602846daa0feec7b37382fed61b001 upstream.
    
    [Why]
    call stack of amdgpu dsc mst pbn, slot num calculation is as below:
    -compute_bpp_x16_from_target_bandwidth
    -decide_dsc_target_bpp_x16
    -setup_dsc_config
    -dc_dsc_compute_bandwidth_range
    -compute_mst_dsc_configs_for_link
    -compute_mst_dsc_configs_for_state
    
    from pbn -> dsc target bpp_x16
    
    bpp_x16 is calulated by compute_bpp_x16_from_target_bandwidth.
    Beside pixel clock and bpp, num_slices_h and bpp_increment_div
    will also affect bpp_x16.
    
    from dsc target bpp_x16 -> pbn
    
    within dm_update_mst_vcpi_slots_for_dsc,
    pbn = drm_dp_calc_pbn_mode(clock, bpp_x16, true);
    
    drm_dp_calc_pbn_mode(int clock, int bpp, bool dsc)
    {
      return DIV_ROUND_UP_ULL(mul_u32_u32(clock * (bpp / 16), 64 * 1006),
                8 * 54 * 1000 * 1000);
    }
    
    bpp / 16 trunc digits after decimal point. This will cause calculation
    delta. drm_dp_calc_pbn_mode does not have other informations,
    like num_slices_h, bpp_increment_div. therefore, it does not do revese
    calcuation properly from bpp_x16 to pbn.
    
    pbn from drm_dp_calc_pbn_mode is less than pbn from
    compute_mst_dsc_configs_for_state. This cause not enough mst slot
    allocated to display. display could not visually light up.
    
    [How]
    pass pbn from compute_mst_dsc_configs_for_state to
    dm_update_mst_vcpi_slots_for_dsc
    
    Cc: stable@vger.kernel.org
    
    Reviewed-by: Scott Foster <Scott.Foster@amd.com>
    Acked-by: Mikita Lipski <mikita.lipski@amd.com>
    Signed-off-by: Hersen Wu <hersenwu@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -483,12 +483,7 @@ struct dsc_mst_fairness_params {
 	uint32_t num_slices_h;
 	uint32_t num_slices_v;
 	uint32_t bpp_overwrite;
-};
-
-struct dsc_mst_fairness_vars {
-	int pbn;
-	bool dsc_enabled;
-	int bpp_x16;
+	struct amdgpu_dm_connector *aconnector;
 };
 
 static int kbps_to_peak_pbn(int kbps)
@@ -715,12 +710,12 @@ static void try_disable_dsc(struct drm_atomic_state *state,
 
 static bool compute_mst_dsc_configs_for_link(struct drm_atomic_state *state,
 					     struct dc_state *dc_state,
-					     struct dc_link *dc_link)
+					     struct dc_link *dc_link,
+					     struct dsc_mst_fairness_vars *vars)
 {
 	int i;
 	struct dc_stream_state *stream;
 	struct dsc_mst_fairness_params params[MAX_PIPES];
-	struct dsc_mst_fairness_vars vars[MAX_PIPES];
 	struct amdgpu_dm_connector *aconnector;
 	int count = 0;
 	bool debugfs_overwrite = false;
@@ -741,6 +736,7 @@ static bool compute_mst_dsc_configs_for_link(struct drm_atomic_state *state,
 		params[count].timing = &stream->timing;
 		params[count].sink = stream->sink;
 		aconnector = (struct amdgpu_dm_connector *)stream->dm_stream_context;
+		params[count].aconnector = aconnector;
 		params[count].port = aconnector->port;
 		params[count].clock_force_enable = aconnector->dsc_settings.dsc_force_enable;
 		if (params[count].clock_force_enable == DSC_CLK_FORCE_ENABLE)
@@ -763,6 +759,7 @@ static bool compute_mst_dsc_configs_for_link(struct drm_atomic_state *state,
 	}
 	/* Try no compression */
 	for (i = 0; i < count; i++) {
+		vars[i].aconnector = params[i].aconnector;
 		vars[i].pbn = kbps_to_peak_pbn(params[i].bw_range.stream_kbps);
 		vars[i].dsc_enabled = false;
 		vars[i].bpp_x16 = 0;
@@ -816,7 +813,8 @@ static bool compute_mst_dsc_configs_for_link(struct drm_atomic_state *state,
 }
 
 bool compute_mst_dsc_configs_for_state(struct drm_atomic_state *state,
-				       struct dc_state *dc_state)
+				       struct dc_state *dc_state,
+				       struct dsc_mst_fairness_vars *vars)
 {
 	int i, j;
 	struct dc_stream_state *stream;
@@ -847,7 +845,7 @@ bool compute_mst_dsc_configs_for_state(struct drm_atomic_state *state,
 			return false;
 
 		mutex_lock(&aconnector->mst_mgr.lock);
-		if (!compute_mst_dsc_configs_for_link(state, dc_state, stream->link)) {
+		if (!compute_mst_dsc_configs_for_link(state, dc_state, stream->link, vars)) {
 			mutex_unlock(&aconnector->mst_mgr.lock);
 			return false;
 		}
