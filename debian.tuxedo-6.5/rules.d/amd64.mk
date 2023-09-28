9cd780f3b0b2464325b8b5c44b6fdeded15bbba1
Werner Sembach <wse@tuxedocomputers.com>
2023-09-28 20:00:25 +0200

    TUXEDO: Fix build error caused by oversight
    On abstracted Debian directory creation the flavor name was not changed by
    accident in one of the build files.
    
    Ignore: yes
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>

--- debian.tuxedo-6.5/rules.d/amd64.mk
+++ debian.tuxedo-6.5/rules.d/amd64.mk
@@ -1,7 +1,7 @@
 human_arch	= 64 bit x86
 build_arch	= x86
 defconfig	= defconfig
-flavours	= generic
+flavours	= tuxedo
 build_image	= bzImage
 kernel_file	= arch/$(build_arch)/boot/bzImage
 install_file	= vmlinuz
