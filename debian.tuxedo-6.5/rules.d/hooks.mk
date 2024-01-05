705fdd0f70ea432ae9bea6864bb1b042d5e35104
Werner Sembach <wse@tuxedocomputers.com>
2024-01-05 18:38:19 +0000

    UBUNTU: Ubuntu-tuxedo-6.5-6.5.0-10017.21
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>

--- debian.tuxedo-6.5/rules.d/hooks.mk
+++ debian.tuxedo-6.5/rules.d/hooks.mk
@@ -1,2 +1,13 @@
 do_enforce_all = true
-gcc = gcc-11
+do_libc_dev_package	= false
+do_doc_package		= false
+do_tools_common		= false
+do_tools_host		= false
+do_lib_rust		= false
+gcc	= gcc-12
+do_libc_dev_package	= false
+do_doc_package		= false
+do_tools_common		= false
+do_tools_host		= false
+do_lib_rust		= false
+gcc	= gcc-12
