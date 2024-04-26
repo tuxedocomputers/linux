8736f0f151edcaf76237887161f63ea50abc9c58
Werner Sembach <wse@tuxedocomputers.com>
2024-04-26 13:34:38 +0200

    TUXEDO: Fix automatic updates scripts code style

--- 03-tuxedo-update-version.sh
+++ 03-tuxedo-update-version.sh
@@ -4,7 +4,6 @@ set -ex
 
 MAINLINE_KERNEL_VERSION=6.8
 UBUNTU_VERSION=24.04
-UBUNTU_CODENAME=noble
 UBUNTU_KERNEL_BRANCH=master
 
 for arg in "$@"; do
@@ -45,49 +44,37 @@ git fetch tuxedo ${TUXEDO_KERNEL_BRANCH_FULL} --tags --prune --force
 echo "===Gather version informations.==="
 
 UBUNTU_MAINLINE_VERSION=${MAINLINE_KERNEL_VERSION}.0
-ESCAPED_UBUNTU_MAINLINE_VERSION=${MAINLINE_KERNEL_VERSION%%.*}\\.${MAINLINE_KERNEL_VERSION##*.}\\.0
-ESCAPED_TUXEDO_KERNEL_BRANCH=tuxedo-${MAINLINE_KERNEL_VERSION%%.*}\\.${MAINLINE_KERNEL_VERSION##*.}
 
-
-NEWEST_TUXEDO_TAG=$(git describe tuxedo/${TUXEDO_KERNEL_BRANCH_FULL} --tags --match "Ubuntu-${ESCAPED_TUXEDO_KERNEL_BRANCH}-*" --abbrev=0)
-
-NEWEST_TUXEDO_ABI_AND_BUILD=${NEWEST_TUXEDO_TAG#Ubuntu-${ESCAPED_TUXEDO_KERNEL_BRANCH}-${ESCAPED_UBUNTU_MAINLINE_VERSION}-}
+NEWEST_TUXEDO_TAG=$(git describe tuxedo/${TUXEDO_KERNEL_BRANCH_FULL} --tags --match "Ubuntu-${TUXEDO_KERNEL_BRANCH}-*" --abbrev=0)
+NEWEST_TUXEDO_ABI_AND_BUILD=${NEWEST_TUXEDO_TAG#Ubuntu-"${TUXEDO_KERNEL_BRANCH}"-"${UBUNTU_MAINLINE_VERSION}"-}
 NEWEST_TUXEDO_ABI=${NEWEST_TUXEDO_ABI_AND_BUILD%%.*}
-NEWEST_TUXEDO_ABI_NUMBER=$((${NEWEST_TUXEDO_ABI} / 1000))
+NEWEST_TUXEDO_ABI_NUMBER=$((NEWEST_TUXEDO_ABI/1000))
 
-
-CURRENT_TUXEDO_VERSION_NUMBER=($(grep --perl-regexp --only-matching --max-count 1 "(?<=^linux-${TUXEDO_KERNEL_BRANCH} \().*(?=\))" debian.${TUXEDO_KERNEL_BRANCH}/changelog))
-
-CURRENT_TUXEDO_ABI_AND_BUILD=${CURRENT_TUXEDO_VERSION_NUMBER#${ESCAPED_UBUNTU_MAINLINE_VERSION}-}
+CURRENT_TUXEDO_VERSION_NUMBER=$(grep --perl-regexp --only-matching --max-count 1 "(?<=^linux-${TUXEDO_KERNEL_BRANCH} \().*(?=\))" debian.${TUXEDO_KERNEL_BRANCH}/changelog)
+CURRENT_TUXEDO_ABI_AND_BUILD=${CURRENT_TUXEDO_VERSION_NUMBER#"${UBUNTU_MAINLINE_VERSION}"-}
 CURRENT_TUXEDO_ABI=${CURRENT_TUXEDO_ABI_AND_BUILD%%.*}
-CURRENT_TUXEDO_ABI_NUMBER=$((${CURRENT_TUXEDO_ABI} / 1000))
-
+CURRENT_TUXEDO_ABI_NUMBER=$((CURRENT_TUXEDO_ABI/1000))
 CURRENT_TUXEDO_BUILD_NUMBER=${CURRENT_TUXEDO_VERSION_NUMBER##*tux}
 
-CURRENT_TUXEDO_TAG_NUMBER=${CURRENT_TUXEDO_VERSION_NUMBER//\~/_}
-CURRENT_TUXEDO_TAG=Ubuntu-${TUXEDO_KERNEL_BRANCH}-${CURRENT_TUXEDO_TAG_NUMBER}
-
-
 if [[ "${UBUNTU_KERNEL_BRANCH}" == "master" ]]; then
-    NEXT_BASE_TAG=$(git describe --tags --match "Ubuntu-${ESCAPED_UBUNTU_MAINLINE_VERSION}-*" --abbrev=0)
+    NEXT_BASE_TAG=$(git describe --tags --match "Ubuntu-${UBUNTU_MAINLINE_VERSION}-*" --abbrev=0)
 else
-    NEXT_BASE_TAG=$(git describe --tags --match "Ubuntu-${UBUNTU_KERNEL_BRANCH}-${ESCAPED_UBUNTU_MAINLINE_VERSION}-*" --abbrev=0)
+    NEXT_BASE_TAG=$(git describe --tags --match "Ubuntu-${UBUNTU_KERNEL_BRANCH}-${UBUNTU_MAINLINE_VERSION}-*" --abbrev=0)
 fi
-NEXT_BASE_TAG_NUMBER=${UBUNTU_MAINLINE_VERSION}-${NEXT_BASE_TAG##*-${ESCAPED_UBUNTU_MAINLINE_VERSION}-}
+NEXT_BASE_TAG_NUMBER=${UBUNTU_MAINLINE_VERSION}-${NEXT_BASE_TAG##*-"${UBUNTU_MAINLINE_VERSION}"-}
 NEXT_BASE_VERSION_NUMBER=${NEXT_BASE_TAG_NUMBER//_/\~}
-NEXT_BASE_ABI_AND_BUILD=${NEXT_BASE_VERSION_NUMBER#${ESCAPED_UBUNTU_MAINLINE_VERSION}-}
+NEXT_BASE_ABI_AND_BUILD=${NEXT_BASE_VERSION_NUMBER#"${UBUNTU_MAINLINE_VERSION}"-}
 NEXT_BASE_ABI_NUMBER=${NEXT_BASE_ABI_AND_BUILD%%.*}
 NEXT_BASE_BUILD=${NEXT_BASE_ABI_AND_BUILD#*.}
 
-
 if [[ ${BUILD} ]]; then
     NEXT_TUXEDO_ABI_NUMBER=${CURRENT_TUXEDO_ABI_NUMBER}
-    NEXT_TUXEDO_BUILD_NUMBER=$((${CURRENT_TUXEDO_BUILD_NUMBER} + 1))
+    NEXT_TUXEDO_BUILD_NUMBER=$((CURRENT_TUXEDO_BUILD_NUMBER+1))
 else
-    NEXT_TUXEDO_ABI_NUMBER=$((${NEWEST_TUXEDO_ABI_NUMBER} + 1))
+    NEXT_TUXEDO_ABI_NUMBER=$((NEWEST_TUXEDO_ABI_NUMBER+1))
     NEXT_TUXEDO_BUILD_NUMBER=1
 fi
-NEXT_TUXEDO_VERSION_NUMBER=${UBUNTU_MAINLINE_VERSION}-$((${NEXT_TUXEDO_ABI_NUMBER} * 1000 + NEXT_BASE_ABI_NUMBER)).${NEXT_BASE_BUILD}tux${NEXT_TUXEDO_BUILD_NUMBER}
+NEXT_TUXEDO_VERSION_NUMBER=${UBUNTU_MAINLINE_VERSION}-$((NEXT_TUXEDO_ABI_NUMBER*1000+NEXT_BASE_ABI_NUMBER)).${NEXT_BASE_BUILD}tux${NEXT_TUXEDO_BUILD_NUMBER}
 NEXT_TUXEDO_TAG_NUMBER=${NEXT_TUXEDO_VERSION_NUMBER//\~/_}
 NEXT_TUXEDO_TAG=Ubuntu-${TUXEDO_KERNEL_BRANCH}-${NEXT_TUXEDO_TAG_NUMBER}
 
@@ -97,7 +84,7 @@ echo "===Update changelog.==="
 if [[ ${DRY} ]]; then
     echo "Dry run. Would execute:"
     echo "    cp debian.${UBUNTU_KERNEL_BRANCH}/changelog debian/changelog"
-    echo "    gbp dch --new-version=${NEXT_TUXEDO_VERSION_NUMBER} --release --since ${NEXT_BASE_TAG} --ignore-branch --spawn-editor=never"
+    echo "    gbp dch --new-version=\"${NEXT_TUXEDO_VERSION_NUMBER}\" --release --since \"${NEXT_BASE_TAG}\" --ignore-branch --spawn-editor=never"
     echo "    if [[ \"$UBUNTU_KERNEL_BRANCH\" == \"master\" ]]; then"
     echo "        awk --include inplace \"NR!=1{print}NR==1{count+=gsub(\\\"^linux \\\",\\\"linux-${TUXEDO_KERNEL_BRANCH} \\\");print}END{if(count!=1)exit 1}\" debian/changelog"
     echo "    else"
@@ -106,7 +93,7 @@ if [[ ${DRY} ]]; then
     echo "    mv debian/changelog debian.${TUXEDO_KERNEL_BRANCH}/changelog"
 else
     cp debian.${UBUNTU_KERNEL_BRANCH}/changelog debian/changelog
-    gbp dch --new-version=${NEXT_TUXEDO_VERSION_NUMBER} --release --since ${NEXT_BASE_TAG} --ignore-branch --spawn-editor=never
+    gbp dch --new-version="${NEXT_TUXEDO_VERSION_NUMBER}" --release --since "${NEXT_BASE_TAG}" --ignore-branch --spawn-editor=never
     if [[ "$UBUNTU_KERNEL_BRANCH" == "master" ]]; then
         awk --include inplace "NR!=1{print}NR==1{count+=gsub(\"^linux \",\"linux-${TUXEDO_KERNEL_BRANCH} \");print}END{if(count!=1)exit 1}" debian/changelog
     else
@@ -122,11 +109,11 @@ if [[ ${DRY} ]]; then
     echo "Dry run. Would execute:"
     echo "    git add debian.${TUXEDO_KERNEL_BRANCH}"
     echo "    git commit --signoff --message \"TUXEDO: ${NEXT_TUXEDO_TAG//_/\~}\" --message \"Gbp-Dch: ignore\""
-    echo "    git tag --sign --message \"${NEXT_TUXEDO_TAG//_/\~}\" ${NEXT_TUXEDO_TAG}"
+    echo "    git tag --sign --message \"${NEXT_TUXEDO_TAG//_/\~}\" \"${NEXT_TUXEDO_TAG}\""
 else
     git add debian.${TUXEDO_KERNEL_BRANCH}
     git commit --signoff --message "TUXEDO: ${NEXT_TUXEDO_TAG//_/\~}" --message "Gbp-Dch: ignore"
-    git tag --sign --message "${NEXT_TUXEDO_TAG//_/\~}" ${NEXT_TUXEDO_TAG}
+    git tag --sign --message "${NEXT_TUXEDO_TAG//_/\~}" "${NEXT_TUXEDO_TAG}"
 fi
 
 echo "===Done.==="
