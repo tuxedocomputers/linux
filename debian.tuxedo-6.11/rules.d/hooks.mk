399698466a17534a8e38d8dfdececbd15e9573bf
Werner Sembach <wse@tuxedocomputers.com>
2024-08-29 13:30:03 +0200

    TUXEDO: Skip tool with dependencies unavailable on jammy
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>

--- debian.tuxedo-6.11/rules.d/hooks.mk
+++ debian.tuxedo-6.11/rules.d/hooks.mk
@@ -3,4 +3,11 @@ do_doc_package		= false
 do_tools_common		= false
 do_tools_host		= false
 do_lib_rust		= false
-gcc	= gcc-12
+
+# gcc-12 is the newest gcc available for jammy. This overwrites the gcc-14
+# default.
+gcc			= gcc-12
+
+# rtls requires newer versions of libtraceevent-dev and libtracefs-dev then
+# available in jammy since kernel 6.9.
+do_tools_rtla		= false
