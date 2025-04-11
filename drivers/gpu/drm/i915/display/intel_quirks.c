7e37ccddf5f8447b3b2cb083690dd1f7057fbdea
Werner Sembach <wse@tuxedocomputers.com>
2025-04-11 17:55:08 +0200

    drm/i915/display: Add quirk to force backlight type on some TUXEDO devices
    The display backlight on TUXEDO DX1708 and InsanityBook 15 v1 with panels
    AUO 12701 and AUO 12701 must be forced to INTEL_DP_AUX_BACKLIGHT_ON to be
    able to control the brightness.
    
    This could already be archived via a module parameter, but this patch adds
    a quirk to apply this by default on the mentioned device + panel
    combinations.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>

--- drivers/gpu/drm/i915/display/intel_quirks.c
+++ drivers/gpu/drm/i915/display/intel_quirks.c
@@ -86,6 +86,12 @@ static void quirk_edp_limit_rate_hbr2(struct intel_display *display)
 	drm_info(display->drm, "Applying eDP Limit rate to HBR2 quirk\n");
 }
 
+static void quirk_auo_12701_21229_enable_dpcd_backlight(struct intel_display *display)
+{
+	intel_set_quirk(display, QUIRK_AUO_12701_21229_ENABLE_DPCD_BACKLIGHT);
+	drm_info(display->drm, "Applying Display AUO Model 12701 and 21229 Enable DPCD Backlight quirk\n");
+}
+
 struct intel_quirk {
 	int device;
 	int subsystem_vendor;
@@ -123,6 +129,12 @@ static int intel_dmi_reverse_brightness(const struct dmi_system_id *id)
 static int intel_dmi_no_pps_backlight(const struct dmi_system_id *id)
 {
 	DRM_INFO("No pps backlight support on %s\n", id->ident);
+	return 1;
+}
+
+static int intel_dmi_auo_12701_21229_enable_dpcd_backlight(const struct dmi_system_id *id)
+{
+	DRM_INFO("Display AUO model 12701 and 21229 DPCD backlight control on %s\n", id->ident);
 	return 1;
 }
 
@@ -171,6 +183,36 @@ static const struct intel_dmi_quirk intel_dmi_quirks[] = {
 			{ }
 		},
 		.hook = quirk_no_pps_backlight_power_hook,
+	},
+	{
+		.dmi_id_list = &(const struct dmi_system_id[]) {
+			{
+				.callback = intel_dmi_auo_12701_21229_enable_dpcd_backlight,
+				.ident = "TUXEDO DX1708",
+				.matches = {DMI_MATCH(DMI_BOARD_NAME, "N8xEJEK"),
+				},
+			},
+			{
+				.callback = intel_dmi_auo_12701_21229_enable_dpcd_backlight,
+				.ident = "TUXEDO InsanityBook 15 v1",
+				.matches = {DMI_MATCH(DMI_BOARD_NAME, "P95_HP"),
+				},
+			},
+			{
+				.callback = intel_dmi_auo_12701_21229_enable_dpcd_backlight,
+				.ident = "TUXEDO InsanityBook 15 v1",
+				.matches = {DMI_MATCH(DMI_BOARD_NAME, "P95_HR"),
+				},
+			},
+			{
+				.callback = intel_dmi_auo_12701_21229_enable_dpcd_backlight,
+				.ident = "TUXEDO InsanityBook 15 v1",
+				.matches = {DMI_MATCH(DMI_BOARD_NAME, "P95_HP,HR,HQ"),
+				},
+			},
+			{ }
+		},
+		.hook = quirk_auo_12701_21229_enable_dpcd_backlight,
 	},
 };
 
