9be432c0933e3e7b0f9ef347b138d06da8bbbfd8
Werner Sembach <wse@tuxedocomputers.com>
2025-04-11 17:55:08 +0200

    drm/i915/display: Add quirk to force backlight type on some TUXEDO devices
    The display backlight on TUXEDO DX1708 and InsanityBook 15 v1 with panels
    AUO 12701 and AUO 12701 must be forced to INTEL_DP_AUX_BACKLIGHT_ON to be
    able to control the brightness.
    
    This could already be archived via a module parameter, but this patch adds
    a quirk to apply this by default on the mentioned device + panel
    combinations.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>

--- drivers/gpu/drm/i915/display/intel_dp_aux_backlight.c
+++ drivers/gpu/drm/i915/display/intel_dp_aux_backlight.c
@@ -34,11 +34,14 @@
  * for some reason.
  */
 
+#include <drm/drm_edid.h>
 #include "i915_drv.h"
+#include "i915_utils.h"
 #include "intel_backlight.h"
 #include "intel_display_types.h"
 #include "intel_dp.h"
 #include "intel_dp_aux_backlight.h"
+#include "intel_quirks.h"
 
 /*
  * DP AUX registers for Intel's proprietary HDR backlight interface. We define
@@ -591,16 +594,27 @@ static const struct intel_panel_bl_funcs intel_dp_vesa_bl_funcs = {
 
 int intel_dp_aux_init_backlight_funcs(struct intel_connector *connector)
 {
+	struct intel_display *display = to_intel_display(connector);
 	struct drm_device *dev = connector->base.dev;
 	struct intel_panel *panel = &connector->panel;
 	struct intel_dp *intel_dp = enc_to_intel_dp(connector->encoder);
 	struct drm_i915_private *i915 = dp_to_i915(intel_dp);
 	bool try_intel_interface = false, try_vesa_interface = false;
+	int enable_dpcd_backlight;
+	u32 pnl_id;
 
 	/* Check the VBT and user's module parameters to figure out which
 	 * interfaces to probe
 	 */
-	switch (i915->display.params.enable_dpcd_backlight) {
+	enable_dpcd_backlight = i915->display.params.enable_dpcd_backlight;
+	if (enable_dpcd_backlight == INTEL_DP_AUX_BACKLIGHT_AUTO &&
+	    intel_has_quirk(display, QUIRK_AUO_12701_21229_ENABLE_DPCD_BACKLIGHT)) {
+		pnl_id = drm_edid_get_panel_id(panel->fixed_edid);
+		if (pnl_id == drm_edid_encode_panel_id('A', 'U', 'O', 0x319d) ||
+		    pnl_id == drm_edid_encode_panel_id('A', 'U', 'O', 0x52ed))
+			enable_dpcd_backlight = INTEL_DP_AUX_BACKLIGHT_ON;
+	}
+	switch (enable_dpcd_backlight) {
 	case INTEL_DP_AUX_BACKLIGHT_OFF:
 		return -ENODEV;
 	case INTEL_DP_AUX_BACKLIGHT_AUTO:
