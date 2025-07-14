3f906c75ff136f93be739b24d5e9b9b6b50d4b27
Werner Sembach <wse@tuxedocomputers.com>
2025-07-14 13:08:56 +0200

    TUXEDO: Small build scripts touchups
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>

--- 02-tuxedo-update-version.sh
+++ 02-tuxedo-update-version.sh
@@ -29,23 +29,23 @@ cd "${SCRIPTPATH}"
 
 echo "===Gather version informations.==="
 
-TUXEDO_VERSION=$(grep --perl-regexp --only-matching --max-count 1 "(?<=^linux \().*(?=\) tuxedo)" debian/changelog || echo 0.0.0-0tux0)
+TUXEDO_VERSION=$(grep --perl-regexp --only-matching --max-count=1 "(?<=^linux \().*(?=\) tuxedo)" debian/changelog || echo 0.0.0-0tux0)
 TUXEDO_ABI=$((${TUXEDO_VERSION##*tux}+1))
 
-BASE_VERSION=$(grep --perl-regexp --only-matching --max-count 1 "(?<=^linux \().*(?=\) unstable)" debian/changelog)
+BASE_VERSION=$(grep --perl-regexp --only-matching --max-count=1 "(?<=^linux \().*(?=\) unstable)" debian/changelog)
 
 
 echo "===Update changelog and commit.==="
 
 if [[ ${DRY} ]]; then
     echo "Dry run. Would execute:"
-    echo "    DEBFULLNAME=\"Tuxedo BOT\" DEBEMAIL=\"tux@tuxedocomputers.com\" gbp dch --new-version \"${BASE_VERSION}tux${TUXEDO_ABI}\" --distribution tuxedo --force-distribution --release --ignore-branch --spawn-editor never"
-    echo "    git commit --signoff --message \"TUXEDO: ${BASE_VERSION}tux${TUXEDO_ABI}\" --message \"Gbp-Dch: ignore\" debian/changelog"
-    echo "    git tag --sign --message \"debian/${BASE_VERSION}tux${TUXEDO_ABI}\" \"debian/${BASE_VERSION}tux${TUXEDO_ABI}\""
+    echo "    DEBFULLNAME=\"Tuxedo BOT\" DEBEMAIL=\"tux@tuxedocomputers.com\" gbp dch --new-version=\"${BASE_VERSION}tux${TUXEDO_ABI}\" --distribution=tuxedo --force-distribution --release --ignore-branch --spawn-editor=never"
+    echo "    git commit --signoff --message=\"TUXEDO: ${BASE_VERSION}tux${TUXEDO_ABI}\" --message=\"Gbp-Dch: ignore\" debian/changelog"
+    echo "    git tag --sign --message=\"debian/${BASE_VERSION}tux${TUXEDO_ABI}\" \"debian/${BASE_VERSION}tux${TUXEDO_ABI}\""
 else
-    DEBFULLNAME="Tuxedo BOT" DEBEMAIL="tux@tuxedocomputers.com" gbp dch --new-version "${BASE_VERSION}tux${TUXEDO_ABI}" --distribution tuxedo --force-distribution --release --ignore-branch --spawn-editor never
-    git commit --signoff --message "TUXEDO: ${BASE_VERSION}tux${TUXEDO_ABI}" --message "Gbp-Dch: ignore" debian/changelog
-    git tag --sign --message "debian/${BASE_VERSION}tux${TUXEDO_ABI}" "debian/${BASE_VERSION}tux${TUXEDO_ABI}"
+    DEBFULLNAME="Tuxedo BOT" DEBEMAIL="tux@tuxedocomputers.com" gbp dch --new-version="${BASE_VERSION}tux${TUXEDO_ABI}" --distribution=tuxedo --force-distribution --release --ignore-branch --spawn-editor=never
+    git commit --signoff --message="TUXEDO: ${BASE_VERSION}tux${TUXEDO_ABI}" --message="Gbp-Dch: ignore" debian/changelog
+    git tag --sign --message="debian/${BASE_VERSION}tux${TUXEDO_ABI}" "debian/${BASE_VERSION}tux${TUXEDO_ABI}"
 fi
 
 echo "===Done.==="
