255cf14fe41039f892966e21447eb273526b9b7d
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-05-21 16:01:16 +0200

    ACPI: NFIT: core: Use devm_acpi_install_notify_handler()
    BugLink: https://bugs.launchpad.net/bugs/2164666
    
    Now that devm_acpi_install_notify_handler() is available, use it in
    acpi_nfit_probe() instead of a custom devm action removing an ACPI
    notify handler installed via acpi_dev_install_notify_handler().
    
    Also drop the explicit ACPI_COMPANION() check against NULL that is
    not necessary any more becuase devm_acpi_install_notify_handler()
    carries out an equivalent check internally and use ACPI_HANDLE() to
    retrieve the platform device's ACPI handle.
    
    No intentional functional impact.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/3048737.e9J7NaK4W3@rafael.j.wysocki
    (cherry picked from commit 198541ad53c0d0d891fedea4098f9953a0f566c0)
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- drivers/acpi/nfit/core.c
+++ drivers/acpi/nfit/core.c
@@ -3300,14 +3300,6 @@ static void acpi_nfit_notify(acpi_handle handle, u32 event, void *data)
 	device_unlock(dev);
 }
 
-static void acpi_nfit_remove_notify_handler(void *data)
-{
-	struct acpi_device *adev = data;
-
-	acpi_dev_remove_notify_handler(adev, ACPI_DEVICE_NOTIFY,
-				       acpi_nfit_notify);
-}
-
 void acpi_nfit_shutdown(void *data)
 {
 	struct acpi_nfit_desc *acpi_desc = data;
@@ -3348,22 +3340,12 @@ static int acpi_nfit_probe(struct platform_device *pdev)
 	struct acpi_nfit_desc *acpi_desc;
 	struct device *dev = &pdev->dev;
 	struct acpi_table_header *tbl;
-	struct acpi_device *adev;
 	acpi_status status = AE_OK;
 	acpi_size sz;
 	int rc = 0;
 
-	adev = ACPI_COMPANION(&pdev->dev);
-	if (!adev)
-		return -ENODEV;
-
-	rc = acpi_dev_install_notify_handler(adev, ACPI_DEVICE_NOTIFY,
-					     acpi_nfit_notify, dev);
-	if (rc)
-		return rc;
-
-	rc = devm_add_action_or_reset(dev, acpi_nfit_remove_notify_handler,
-					adev);
+	rc = devm_acpi_install_notify_handler(dev, ACPI_DEVICE_NOTIFY,
+					      acpi_nfit_notify, dev);
 	if (rc)
 		return rc;
 
@@ -3394,7 +3376,7 @@ static int acpi_nfit_probe(struct platform_device *pdev)
 	acpi_desc->acpi_header = *tbl;
 
 	/* Evaluate _FIT and override with that if present */
-	status = acpi_evaluate_object(adev->handle, "_FIT", NULL, &buf);
+	status = acpi_evaluate_object(ACPI_HANDLE(dev), "_FIT", NULL, &buf);
 	if (ACPI_SUCCESS(status) && buf.length > 0) {
 		union acpi_object *obj = buf.pointer;
 
