d0dc7ccceb671ec0e6de6bf923e506ddd7443ff1
Ratheesh Kannoth <rkannoth@marvell.com>
2026-06-15 09:01:57 +0530

    octeontx2-af: npc: Log successful MCAM drop-on-non-hit install at debug level
    BugLink: https://bugs.launchpad.net/bugs/2164666
    
    [ Upstream commit 4f6ac65e81625165257131ec2574bb6bb09bd7d8 ]
    
    npc_install_mcam_drop_rule() used dev_err() after a successful
    rvu_mbox_handler_npc_mcam_write_entry() call, so normal installs appeared
    as errors in dmesg.  Use dev_dbg() for the success path and keep dev_err()
    for real failures.
    
    Fixes: 3571fe07a090 ("octeontx2-af: Drop rules for NPC MCAM")
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20260615033157.535237-1-rkannoth@marvell.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [cremfuelled: manual merge]
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc_fs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc_fs.c
@@ -1789,7 +1789,8 @@ int npc_install_mcam_drop_rule(struct rvu *rvu, int mcam_idx, u16 *counter_idx,
 		return err;
 	}
 
-	dev_err(rvu->dev, "%s: Installed single drop on non hit rule at %d, cntr=%d\n",
+	dev_dbg(rvu->dev,
+		"%s: Installed single drop on non hit rule at %d, cntr=%d\n",
 		__func__, mcam_idx, req.cntr);
 
 	/* disable entry at Bank 0, index 0 */
