c1bea34e0469dc2987f4ea21b56dc8577f3fd384
Muchun Song <songmuchun@bytedance.com>
2026-07-24 06:49:05 -0400

    mm/sparse-vmemmap: pass @pgmap argument to memory deactivation paths
    BugLink: https://bugs.launchpad.net/bugs/2165189
    
    [ Upstream commit 3bbc54dd1b62f1a4b218c70aafbeceeba7c90c5d ]
    
    Currently, the memory hot-remove call chain -- arch_remove_memory(),
    __remove_pages(), sparse_remove_section() and section_deactivate() -- does
    not carry the struct dev_pagemap pointer.  This prevents the lower levels
    from knowing whether the section was originally populated with vmemmap
    optimizations (e.g., DAX with vmemmap optimization enabled).
    
    Without this information, we cannot call vmemmap_can_optimize() to
    determine if the vmemmap pages were optimized.  As a result, the vmemmap
    page accounting during teardown will mistakenly assume a non-optimized
    allocation, leading to incorrect memmap statistics.
    
    To lay the groundwork for fixing the vmemmap page accounting, we need to
    pass the @pgmap pointer down to the deactivation location.  Plumb the
    @pgmap argument through the APIs of arch_remove_memory(), __remove_pages()
    and sparse_remove_section(), mirroring the corresponding *_activate()
    paths.
    
    Link: https://lore.kernel.org/20260428081855.1249045-4-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Acked-by: Liam R. Howlett <liam@infradead.org>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@linux.ibm.com>
    Cc: Joao Martins <joao.m.martins@oracle.com>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Stable-dep-of: 721a73e30c9e ("mm/sparse-vmemmap: fix DAX vmemmap accounting with optimization")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [cremfuelled: sparse-vmemmap.c => sparse.c]
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- mm/sparse.c
+++ mm/sparse.c
@@ -679,7 +679,7 @@ static struct page * __meminit populate_section_memmap(unsigned long pfn,
 }
 
 static void depopulate_section_memmap(unsigned long pfn, unsigned long nr_pages,
-		struct vmem_altmap *altmap)
+		struct vmem_altmap *altmap, struct dev_pagemap *pgmap)
 {
 	unsigned long start = (unsigned long) pfn_to_page(pfn);
 	unsigned long end = start + nr_pages * sizeof(struct page);
@@ -832,7 +832,7 @@ static int fill_subsection_map(unsigned long pfn, unsigned long nr_pages)
  * For 2 and 3, the SPARSEMEM_VMEMMAP={y,n} cases are unified
  */
 static void section_deactivate(unsigned long pfn, unsigned long nr_pages,
-		struct vmem_altmap *altmap)
+		struct vmem_altmap *altmap, struct dev_pagemap *pgmap)
 {
 	struct mem_section *ms = __pfn_to_section(pfn);
 	bool section_is_early = early_section(ms);
@@ -872,7 +872,7 @@ static void section_deactivate(unsigned long pfn, unsigned long nr_pages,
 	 * section_activate() and pfn_valid() .
 	 */
 	if (!section_is_early)
-		depopulate_section_memmap(pfn, nr_pages, altmap);
+		depopulate_section_memmap(pfn, nr_pages, altmap, pgmap);
 	else if (memmap)
 		free_map_bootmem(memmap);
 
@@ -916,7 +916,7 @@ static struct page * __meminit section_activate(int nid, unsigned long pfn,
 
 	memmap = populate_section_memmap(pfn, nr_pages, nid, altmap, pgmap);
 	if (!memmap) {
-		section_deactivate(pfn, nr_pages, altmap);
+		section_deactivate(pfn, nr_pages, altmap, pgmap);
 		return ERR_PTR(-ENOMEM);
 	}
 
@@ -978,13 +978,13 @@ int __meminit sparse_add_section(int nid, unsigned long start_pfn,
 }
 
 void sparse_remove_section(unsigned long pfn, unsigned long nr_pages,
-			   struct vmem_altmap *altmap)
+		struct vmem_altmap *altmap, struct dev_pagemap *pgmap)
 {
 	struct mem_section *ms = __pfn_to_section(pfn);
 
 	if (WARN_ON_ONCE(!valid_section(ms)))
 		return;
 
-	section_deactivate(pfn, nr_pages, altmap);
+	section_deactivate(pfn, nr_pages, altmap, pgmap);
 }
 #endif /* CONFIG_MEMORY_HOTPLUG */
